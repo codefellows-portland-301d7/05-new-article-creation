240eb965e4c0f54bf9c5698f270e750f391b3719
meghanroserebecca <meghanroserebecca@gmail.com>
2016-12-20 14:09:40 -0800

    have seemingly coded all required functionality

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -3,11 +3,11 @@ var articleView = {};
 
 articleView.render = function() {
   articles.forEach(function(a) {
-    $('#articles').append(a.toHtml('#article-template'));
+    /*$('#articles').append(a.toHtml('#article-template'));
     $('#author-filter').append(a.toHtml('#author-filter-template'));
     if($('#category-filter option:contains("'+ a.category + '")').length === 0) {
       $('#category-filter').append(a.toHtml('#category-filter-template'));
-    };
+    };*/
   });
   $('pre code').each(function(i, block) {
     hljs.highlightBlock(block);
@@ -65,8 +65,43 @@ articleView.setTeasers = function() {
   });
 };
 
+articleView.initNewArticlePage = function() {
+  $('.tab-content').show();
+  $('#export-field').hide();
+  $('#article-json').on('click', function() {
+    $(this).select();
+  });
+  $('new-form').on('change', articleView.create);
+};
+
+articleView.create = function() {
+  $('#article-preview').empty();
+
+  var formArticle = new Article({
+    title: $('#article-title').val(),
+    author: $('#article-author').val(),
+    authorUrl: $('#article-author-url').val(),
+    body: $('#article-body').val(),
+    publishedOn: $('#article-published:checked').length ? new Date(): null
+  });
+
+  console.log(formArticle);
+
+  $('#article-preview').append(formArticle.toHtml('#article-template'));
+
+  $('pre code').each(function(i, block){
+    hljs.highlightBlock(block);
+  });
+
+  $('#export-field').show();
+  $('#article-json').val(JSON.stringify(formArticle) + ',');
+};
+
+$('#new-form').on('change', articleView.create);
+
 articleView.render();
 articleView.handleCategoryFilter();
 articleView.handleAuthorFilter();
 articleView.handleMainNav();
 articleView.setTeasers();
+articleView.initNewArticlePage();
